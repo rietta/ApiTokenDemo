a2c66163a915fb081c4bfbb8b28c380ef2fe344c
Frank Rietta <frank@rietta.com>
2014-06-01 23:37:08 -0400

    The API token generation and verification.

--- README.md
+++ README.md
@@ -6,6 +6,19 @@ It's available free to the public at https://github.com/rietta/ApiTokenDemo.
 
 This is an example that was not TTD. That is left as an exercise to the reader.
 
+## Generate a token
+```ruby
+  puts ApiToken.create.generated_token
+```
+
+That will generate a new token, persist it to the database, and return the generated token once. Every call will generate a brand new token. The generated_token is not persisted to the database in plaintext.
+
+The generated token will look like this:
+
+```
+eac4d6c04e6f4658977fbcc1448c6ae3:096fabb7192b99fc817c30f3cecc4c5c06583f338be0efe2d6e69027c532977c2b48fb0d4a785d3c8cc0651989cff5f19027eb2a35a96a65
+```
+
 ## Further reading
 
 - [Ruby on Rails 4.1.1: ActiveModel::SecurePassword::ClassMethods](http://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html)
--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -20,12 +20,39 @@ class ApiToken < ActiveRecord::Base
 
   def initialize(opts = {})
     super(opts)
+
+    # Generate a long, RANDOM_LENGTH * 2 random password string
     generated_password = SecureRandom.hex(RANDOM_LENGTH)
+
     # Create a UUID v4 without dashes, which is guarunteed to be unique as per
     # See http://www.ietf.org/rfc/rfc4122.txt
     self.identifier = SecureRandom.uuid().gsub('-', '')
     self.generated_token = "#{identifier}:#{generated_password}".strip
-    self.password = generated_password
+
+    # We could use the password portion, but including the username gives this a little
+    # extra salt and makes the verifier easier since we can pass in the whole string
+    # to the has_secure_password bcrypt-based authenticate method.
+    self.password = generated_token
   end
 
-end
+  ####################################################
+  # Finders and its helper
+
+  def self.api_key_from_token(token)
+    to_return = (token and token.length >= 145) ? token.split(':').first : nil
+    raise ActiveRecord::RecordNotFound if to_return.nil?
+    to_return
+  end
+
+  # Find the token in the database by the identifier and compare the password hash
+  # using the authenticate method that is supplied by has_secure_password.
+  def self.find_by_token(token)
+    api_token = ApiToken.find_by_identifier(api_key_from_token(token))
+    if api_token and api_token.authenticate(token)
+      return api_token
+    else
+      return nil
+    end
+  end # find_by_token
+
+end ApiToken
